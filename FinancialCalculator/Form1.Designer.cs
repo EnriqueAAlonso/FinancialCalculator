dfc79ca049643c2c5709a119d50e912d75363c04
ManuGlez1 <0192883@up.edu.mx>
2018-11-14 17:37:32 -0600

    Agregue el label de bienvenida, y 2 botones, cada uno va a conducir a una pantalla distinta. OJO: se agrego a la form un tooltip para poder mostrar un pequeño mensaje cuando el usuario su posiciona arriba de uno de nuestros dos botones.

--- FinancialCalculator/Form1.Designer.cs
+++ FinancialCalculator/Form1.Designer.cs
@@ -28,39 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.myTips = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(321, 356);
+            this.button1.Location = new System.Drawing.Point(92, 133);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(91, 58);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calculos individuales";
+            this.myTips.SetToolTip(this.button1, "En esta sección se tendrá la posibilidad de realizar calculos financieros.");
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(268, 217);
+            this.button2.Location = new System.Drawing.Point(254, 133);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(96, 58);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Comparacion";
+            this.myTips.SetToolTip(this.button2, "En esta sección se tendrá la posibilidad de comparar el rendimiento de varios pla" +
+        "nes financieros.");
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(86, 33);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(264, 32);
+            this.lblWelcome.TabIndex = 2;
+            this.lblWelcome.Text = "Bienvenido Extraño";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(412, 309);
+            this.Controls.Add(this.lblWelcome);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +86,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip myTips;
+        private System.Windows.Forms.Label lblWelcome;
     }
 }
 
